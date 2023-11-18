1bfeae4e0cf8ad38a40d44483b1bdbd381c18e70
Aleksei Arro <polsck@gmail.com>
2023-11-18 19:31:37 +0400

    feat: adjust core rules for scss plugin

--- scss.js
+++ scss.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const { reports } = require('./partials');
-const { rules: baseRules } = require('./rules/core');
-const { rules: scssRules } = require('./rules/scss');
+const { rules: coreRulesAll } = require('./rules/core');
+const {
+  rules: scssRules,
+  coreRulesAdaptedForScssPlugin,
+} = require('./rules/scss');
 
-module.exports = {
+const scssConfig = {
   ...reports,
   customSyntax: 'postcss-scss',
   plugins: ['stylelint-scss'],
   rules: {
-    ...baseRules,
+    ...coreRulesAll,
+    ...coreRulesAdaptedForScssPlugin,
     ...scssRules,
   },
 };
+
+module.exports = scssConfig;
